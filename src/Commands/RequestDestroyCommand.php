4cf836d3c1fbdedc8f472a9dbe1ab45283b5f393
Muhammad Azzuhair <muhammadazzuhair@hotmail.com>
2024-03-04 06:46:57 +0700

    Update RequestDestroyCommand.php

--- src/Commands/RequestDestroyCommand.php
+++ src/Commands/RequestDestroyCommand.php
@@ -30,29 +30,6 @@ class RequestDestroyCommand extends DestroyerCommand
     protected $type = 'Request';
 
     /**
-     * Get the stub file for the generator.
-     *
-     * @return string
-     */
-    protected function getStub()
-    {
-        return $this->resolveStubPath('/stubs/request.stub');
-    }
-
-    /**
-     * Resolve the fully-qualified path to the stub.
-     *
-     * @param  string  $stub
-     * @return string
-     */
-    protected function resolveStubPath($stub)
-    {
-        return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
-                        ? $customPath
-                        : __DIR__.$stub;
-    }
-
-    /**
      * Get the default namespace for the class.
      *
      * @param  string  $rootNamespace
