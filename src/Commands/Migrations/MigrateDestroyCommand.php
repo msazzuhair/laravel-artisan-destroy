81ac2bb327f3829c435fe0e4057061e5a0dc36f6
Muhammad Azzuhair <muhammadazzuhair@hotmail.com>
2024-03-04 06:46:08 +0700

    Add migration destroyer commands

---
+++ src/Commands/Migrations/MigrateDestroyCommand.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace Msazzuhair\LaravelArtisanDestroy\Commands\Migrations;
+
+use Illuminate\Contracts\Console\PromptsForMissingInput;
+use Illuminate\Database\Console\Migrations\BaseCommand;
+use Illuminate\Database\Console\Migrations\TableGuesser;
+use Illuminate\Support\Composer;
+use Illuminate\Support\Str;
+use Symfony\Component\Finder\Finder;
+use function Laravel\Prompts\suggest;
+
+class MigrateDestroyCommand extends BaseCommand implements PromptsForMissingInput
+{
+    /**
+     * The console command signature.
+     *
+     * @var string
+     */
+    protected $signature = 'destroy:migration {name : The name of the migration}
+        {--force : Force the operation to run without confirmation}
+        {--path= : The location of the migration file}
+        {--realpath : Indicate any provided migration file paths are pre-resolved absolute paths}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete a migration file';
+
+    /**
+     * The migration creator instance.
+     *
+     * @var \Msazzuhair\LaravelArtisanDestroy\Commands\Migrations\MigrationDestroyer
+     */
+    protected $destroyer;
+
+    /**
+     * The Composer instance.
+     *
+     * @var \Illuminate\Support\Composer
+     *
+     * @deprecated Will be removed in a future Laravel version.
+     */
+    protected $composer;
+
+    /**
+     * Create a new migration install command instance.
+     *
+     * @param  \Msazzuhair\LaravelArtisanDestroy\Commands\Migrations\MigrationDestroyer  $destroyer
+     * @param  \Illuminate\Support\Composer  $composer
+     * @return void
+     */
+    public function __construct(MigrationDestroyer $destroyer, Composer $composer)
+    {
+        parent::__construct();
+
+        $this->destroyer = $destroyer;
+        $this->composer = $composer;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // It's possible for the developer to specify the tables to modify in this
+        // schema operation. The developer may also specify if this table needs
+        // to be freshly created so we can create the appropriate migrations.
+        $name = Str::snake(trim($this->input->getArgument('name')));
+
+        $migration = null;
+        $possibleMigrations = $this->possibleMigrations();
+        if (count($possibleMigrations) === 0) {
+            $this->components->info('No migrations found.');
+            return false;
+        } else if (count($possibleMigrations) === 1) {
+            $migration = $possibleMigrations[0];
+        } else {
+            $migration = $this->choice(
+                'What is the name of the migration file that should be deleted?',
+                $this->possibleMigrations(),
+            );
+        }
+
+        if (! $this->confirmToProceed($this->getMigrationPath() . '/' . $migration . '.php')) {
+            return false;
+        }
+
+        $this->deleteMigration($migration);
+    }
+
+    /**
+     * Write the migration file to disk.
+     *
+     * @param string $name
+     * @param string $table
+     * @param bool $create
+     * @return void
+     * @throws \Exception
+     */
+    protected function deleteMigration($name)
+    {
+        $file = $this->destroyer->delete(
+            $name, $this->getMigrationPath()
+        );
+
+        $this->components->info(sprintf('Migration [%s] deleted successfully.', $file));
+    }
+
+    /**
+     * Get migration path (either specified by '--path' option or default location).
+     *
+     * @return string
+     */
+    protected function getMigrationPath()
+    {
+        if (! is_null($targetPath = $this->input->getOption('path'))) {
+            return ! $this->usingRealPath()
+                            ? $this->laravel->basePath().'/'.$targetPath
+                            : $targetPath;
+        }
+
+        return parent::getMigrationPath();
+    }
+
+    /**
+     * Get a list of possible migration names.
+     *
+     * @return array<int, string>
+     */
+    protected function possibleMigrations()
+    {
+        $migrationPath = $this->getMigrationPath();
+
+        if (! is_dir($migrationPath)) {
+            return [];
+        }
+
+        $name = Str::snake(trim($this->input->getArgument('name')));
+
+        return collect(Finder::create()->files()->name('/' . $name . '/')->depth(0)->in($migrationPath))
+            ->map(fn ($file) => $file->getBasename('.php'))
+            ->sort()
+            ->values()
+            ->all();
+    }
+
+    /**
+     * Prompt for missing input arguments using the returned questions.
+     *
+     * @return array
+     */
+    protected function promptForMissingArgumentsUsing()
+    {
+        return [
+            'name' => ['What is the name of the migration file (without the timestamp)?', $this->possibleMigrations()],
+        ];
+    }
+
+    protected function confirmToProceed($path)
+    {
+        // If force option is used, we don't need confirmation
+        if ($this->option('force')) {
+            return true;
+        } elseif (config('artisan-destroy.confirmation', true)) {
+            if (config('artisan-destroy.git-tracking-confirmation', true)) {
+                // Check if git is present
+                if (! is_dir(base_path().'/.git')) {
+                    $this->error('Git is not initialized in this project. Any deletion will be irreversible.');
+                }
+
+                // Check if the file is tracked by git
+                elseif (shell_exec('git -C '.base_path().' ls-files '.$path) == '') {
+                    $this->error('The file is not tracked by git. Any deletion will be irreversible.');
+                }
+
+                // Check if the file has uncommited changes
+                elseif (shell_exec('git -C '.base_path().' diff --name-only '.$path) != '') {
+                    $this->error('The file has uncommited changes. Any deletion will be irreversible.');
+                }
+            }
+
+            $confirmation = $this->confirm(
+                'Are you sure you want to proceed?'
+            );
+
+            if (! $confirmation) {
+                $this->error('Aborted.');
+            }
+
+            return $confirmation;
+        }
+
+        return false;
+    }
+}
---
+++ src/Commands/Migrations/MigrationDestroyer.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Msazzuhair\LaravelArtisanDestroy\Commands\Migrations;
+
+use Closure;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
+use InvalidArgumentException;
+
+class MigrationDestroyer
+{
+    /**
+     * The filesystem instance.
+     *
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    protected $files;
+
+    /**
+     * The registered post create hooks.
+     *
+     * @var array
+     */
+    protected $postCreate = [];
+
+    /**
+     * Create a new migration creator instance.
+     *
+     * @param  \Illuminate\Filesystem\Filesystem  $files
+     * @param  string  $customStubPath
+     * @return void
+     */
+    public function __construct(Filesystem $files)
+    {
+        $this->files = $files;
+    }
+
+    /**
+     * Create a new migration at the given path.
+     *
+     * @param  string  $name
+     * @param  string  $path
+     * @param  string|null  $table
+     * @param  bool  $create
+     * @return string
+     *
+     * @throws \Exception
+     */
+    public function delete($name, $path)
+    {
+        $this->ensureMigrationAlreadyExists($name, $path);
+
+        $path = $this->getPath($name, $path);
+
+        $this->files->delete($path);
+
+        // Next, we will fire any hooks that are supposed to fire after a migration is
+        // created. Once that is done we'll be ready to return the full path to the
+        // migration file so it can be used however it's needed by the developer.
+        // $this->firePostCreateHooks($table, $path);
+
+        return $path;
+    }
+
+    /**
+     * Ensure that a migration with the given name doesn't already exist.
+     *
+     * @param string $name
+     * @param string $migrationPath
+     * @return void
+     *
+     * @throws \InvalidArgumentException
+     * @throws FileNotFoundException
+     */
+    protected function ensureMigrationAlreadyExists($name, $migrationPath = null)
+    {
+        if (! empty($migrationPath)) {
+            $migrationFiles = $this->files->glob($migrationPath.'/*.php');
+
+            foreach ($migrationFiles as $migrationFile) {
+                $this->files->requireOnce($migrationFile);
+            }
+        }
+    }
+
+    /**
+     * Get the class name of a migration name.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function getClassName($name)
+    {
+        return Str::studly($name);
+    }
+
+    /**
+     * Get the full path to the migration.
+     *
+     * @param  string  $name
+     * @param  string  $path
+     * @return string
+     */
+    protected function getPath($name, $path)
+    {
+        return $path.'/'.$name.'.php';
+    }
+
+    /**
+     * Fire the registered post create hooks.
+     *
+     * @param  string|null  $table
+     * @param  string  $path
+     * @return void
+     */
+    protected function firePostCreateHooks($table, $path)
+    {
+        foreach ($this->postCreate as $callback) {
+            $callback($table, $path);
+        }
+    }
+
+    /**
+     * Register a post migration create hook.
+     *
+     * @param  \Closure  $callback
+     * @return void
+     */
+    public function afterCreate(Closure $callback)
+    {
+        $this->postCreate[] = $callback;
+    }
+
+    /**
+     * Get the date prefix for the migration.
+     *
+     * @return string
+     */
+    protected function getDatePrefix()
+    {
+        return date('Y_m_d_His');
+    }
+
+    /**
+     * Get the filesystem instance.
+     *
+     * @return \Illuminate\Filesystem\Filesystem
+     */
+    public function getFilesystem()
+    {
+        return $this->files;
+    }
+}
